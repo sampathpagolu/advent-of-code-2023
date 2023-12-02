1c756bb2c015cb9478a0ec7121a6e03906b2e704
Sampath Pagolu <sampath.pagolu@onequext.com>
2023-12-02 15:45:21 -0600

    Day-2 part-2

---
+++ 2/cube_game_p2.go
@@ -0,0 +1,80 @@
+package main
+
+
+import (
+    "fmt"
+    "log"
+    "os"
+    "bufio"
+    "strings"
+    "strconv"
+)
+
+var debug bool = false
+
+func main() {
+
+    if !debug {
+        content, err := os.Open("input.txt")
+        if err != nil {
+            log.Fatal(err)
+            return 
+        }
+        defer content.Close()
+        scanner := bufio.NewScanner(content)
+        sum := 0
+        for scanner.Scan() && len(scanner.Text()) > 0{
+            red, green, blue :=   parseInputString(scanner.Text())
+            sum += powerOfFewestNumberOfCubes(red, green, blue)
+        }
+        fmt.Println(sum)
+    } else {
+        red, green, blue := parseInputString("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red")
+        fmt.Println(powerOfFewestNumberOfCubes(red, green, blue))
+    }
+}
+func powerOfFewestNumberOfCubes(red int, green int, blue int) int{
+    
+    return red * blue * green
+}
+
+func parseInputString(txt string) (int, int, int){
+    biggestRed, biggestGreen, biggestBlue := 0,0,0
+    _1 := strings.Split(txt, ":")
+    draws := strings.Split(_1[1], ";")
+    for _, draw := range draws {
+        cubes := strings.Split(draw, ",")
+
+        for _, cube := range cubes {
+
+            cube_split := strings.Split(cube, " ")
+            c := []string{}
+            for i := 0; i < len(cube_split); i++ {
+                if len(cube_split[i]) > 0 {
+                    c = append(c, cube_split[i])
+                }
+            }
+            num, err := strconv.Atoi(c[0])
+            if  err == nil {
+                if strings.Contains(c[1], "red") {
+                    if num > biggestRed {
+                        biggestRed = num
+                    }
+                }
+                if strings.Contains(c[1],"green")  {
+                    if num > biggestGreen {
+                        biggestGreen = num
+                    }
+                }
+                if  strings.Contains(c[1],"blue"){
+                    if num > biggestBlue {
+                        biggestBlue = num
+                    }
+                }
+            }
+        }
+    }
+   // fmt.Printf("gameId: %v, red: %v, green: %v, blue: %v\n", gameId, red, green, blue)
+    return biggestRed, biggestGreen, biggestBlue 
+}
+
